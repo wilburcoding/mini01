6b4ccc4d5cb0b91d7bbd8f8ff365240bde7e810f
Wilbur W <wilburwei.2008@gmail.com>
2025-07-12 19:55:36 -0400

    Implement community detection algorithm

--- mini01.jl
+++ mini01.jl
@@ -4,6 +4,7 @@ using Makie.Colors
 using Random
 using StatsBase
 using Colors
+using Dates
 
 mutable struct NodeInfo
     label::Int
@@ -27,7 +28,40 @@ function label_propagation(g, node_info)
     end
 end
 
-function main(filename = "./graph02.txt")
+function community_detection(g, node_info)
+    changed = true
+    while changed
+        changed = false
+        shuffled_nodes = shuffle(1:nv(g))
+        for u in shuffled_nodes
+            scores = Dict{Int,Float16}()
+            palette_size = 16
+            labels = unique([node.label for node in values(node_info)])
+            label_to_color_index = Dict(labels[i] => mod1(i, palette_size) for i in eachindex(labels))
+            node_color_indices = [label_to_color_index[node_info[n].label] for n in 1:nv(g)]
+            current_score = get_score(g, node_info, node_color_indices)
+            for neighbor in node_info[u].neighbors
+                old_label = node_info[u].label
+                node_info[u].label = node_info[neighbor].label
+                node_color_indices[u] = node_color_indices[neighbor]
+                scores[node_info[neighbor].label] = get_score(g, node_info, node_color_indices)
+                node_info[u].label = old_label
+            end
+            max_score = maximum(values(scores))
+            if max_score > current_score
+                node_info[u].label = argmax(scores)
+                changed = true
+            end
+        end
+    end
+end
+
+        
+
+function main(filename = "./graph03.txt")
+    current_time_ns = time_ns()
+    current_timestamp = Int64(current_time_ns)
+
     edge_list = read_edges(filename)
     g = build_graph(edge_list)
 
@@ -36,9 +70,9 @@ function main(filename = "./graph02.txt")
     for n in 1:nv(g)
         node_info[n] = NodeInfo(n, collect(neighbors(g, n)))
     end
-    
+    community_detection(g, node_info)
     # Run label propagation
-    label_propagation(g, node_info)
+    # label_propagation(g, node_info)
 
     # Use a fixed-size color palette for cycling, e.g., 16 colors
     palette_size = 16
@@ -51,11 +85,12 @@ function main(filename = "./graph02.txt")
     node_colors = [color_palette[i] for i in node_color_indices]
     node_text_colors = [Colors.Lab(RGB(c)).l > 50 ? :black : :white for c in node_colors]
 
-    interactive_plot_graph(g, node_info, node_colors, node_text_colors, node_color_indices, color_palette)
+    #interactive_plot_graph(g, node_info, node_colors, node_text_colors, node_color_indices, color_palette)
 
     # Report the score
     score = get_score(g, node_info, node_color_indices)
     println("score is $score")
+    println("elapsed time is $((time_ns() - current_time_ns) / 1e6) ms")
 end
 
 main()
