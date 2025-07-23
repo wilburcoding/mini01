192c0f7d9ff76e3b228fb1c9c7e3058f5a32fd40
Wilbur W <wilburwei.2008@gmail.com>
2025-07-22 22:14:24 -0400

    Fix modularity

--- scoring.jl
+++ scoring.jl
@@ -1,28 +1,26 @@
 function get_score(g, weights, node_info, node_color_indices)
     total_weight = 0 
     for (u, v) in weights
-        total_weight += v
+        total_weight += v/2
     end
+    println(total_weight)
     total = 0
-    for (vertex, value) in node_info
-        for neighbor in value.neighbors
-            if neighbor > vertex
-                edge_weight = weights[(vertex, neighbor)]
-                k_i = 0
-                for neighbor2 in value.neighbors
-                    k_i += weights[(vertex, neighbor2)]
-                end
-                k_j = 0
-                for neighbor2 in node_info[neighbor].neighbors
-                    k_j += weights[(neighbor, neighbor2)]
-                end
-                same_community = node_color_indices[vertex] == node_color_indices[neighbor] ? 1 : 0
-                total += (edge_weight - ((k_i * k_j) / (2 * total_weight))) * (same_community)
-            end
-
-            
+    for (u, v) in weights
+        edge_weight = v
+        k_i = 0
+        for neighbor in node_info[u[1]].neighbors
+            k_i += weights[(u[1], neighbor)]
         end
+        k_j = 0
+        for neighbor in node_info[u[2]].neighbors
+            k_j += weights[(u[2], neighbor)]
+        end
+        same_community = node_color_indices[u[1]] == node_color_indices[u[2]] ? 1 : 0
+        total += (edge_weight - ((k_i * k_j) / (2 * total_weight))) * (same_community)
     end
+
+
+
 
     # maxscore = 0
     # score = 0
